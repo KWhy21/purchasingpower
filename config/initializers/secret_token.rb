4f0c5206da899e90f35494024c0b7c9af4bbb26f
Kris Young <kyoung9@hotmail.com>
2013-02-05 22:24:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purchasingpower::Application.config.secret_token = '53741adce0e36c9630da83ae27396e48d052b9cb2ecc42ea346adaaac798ca353cca86d79a624a1685447123675769b62b2398d4816f5c892f969c57cc46a4c8'
